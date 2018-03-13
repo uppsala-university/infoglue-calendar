b5994e069ebc808277ab1c06caab96353ecc3800
ioVulture <rasmus.ljungstrom@uadm.uu.se>
2018-03-13 11:16:26 +0100

    IG-4486 added boolean

--- src/org/infoglue/calendar/actions/CalendarAbstractAction.java
+++ src/org/infoglue/calendar/actions/CalendarAbstractAction.java
@@ -746,8 +746,10 @@ public class CalendarAbstractAction extends ActionSupport
 		dateTimeSB.append("<span class='dtstart'>");
 		dateTimeSB.append(startDate);
 		
+		boolean showStartTime = !shortList ? startDate.equalsIgnoreCase(endDate) : false;
+		
 		/* If the time is 12:34 it means that start date was left empty and should not be shown. For short lists, start time should never be shown if the event streches over several days */
-		if (startHourMinute != null && !startHourMinute.equalsIgnoreCase("12" + timeSeparatorNotation + "34") && (!startDate.equalsIgnoreCase(endDate) && !shortList)) {
+		if (startHourMinute != null && !startHourMinute.equalsIgnoreCase("12" + timeSeparatorNotation + "34") && showStartTime) {
 			if (!shortList) {
 				dateTimeSB.append(",");
 			}
